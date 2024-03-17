e3087a7d8d627574e2559c81ec4e0765993434e1
muctepx <daniil.chuprin@gmail.com>
2024-03-17 17:52:17 +0300

    Добавьте файлы проекта.

---
+++ FindFileAndText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem8
+{
+
+    // Объедините две предыдущих работы(практические работы 2 и 3):
+    // поиск файла и поиск текста в файле написав утилиту которая ищет
+    // файлы определенного расширения с указанным текстом.Рекурсивно.
+    // Пример вызова утилиты: utility.exe txt текст.
+    public class FindFileAndText
+    {
+
+        public static void SearchFileAndText(string directory, string extension, string text)
+        {
+            string[] files = Directory.GetFiles(directory, "*" + extension, SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                if (File.ReadLines(file).Any(line => line.Contains(text)))
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Sem8
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            FindFileAndText.SearchFileAndText("d:\\Education", ".txt", "деньги");
+
+            /*
+             *  //1. Напишите консольную утилиту для копирования файлов 
+        // Пример запуска: utility.exe file1.txt file2.txt
+                        FindFileAndText.SearchFile(args[0], args[1], args[2]);
+
+                        foreach (var line in FindFileAndText.list)
+                        {
+                            Console.WriteLine(line);
+                        }
+
+                         * Напишите утилиту читающую тестовый файл и выводящую на экран строки содержащие искомое слово.
+                        using (StreamReader sr = new StreamReader(args[0]))
+                        {
+                            while (!sr.EndOfStream)
+                            {
+                                var tempString = sr.ReadLine();
+                                if (tempString.Contains(args[1]))
+                                {
+                                    Console.WriteLine(tempString);
+                                }
+                            }
+                        }
+
+                    }
+                        /*
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("Нет параметров для запуска");
+                        }
+                        else
+                        {
+                            if (!File.Exists(args[0]))
+                            {
+                                using (StreamWriter sw = new StreamWriter(args[0]))
+                                {
+                                    sw.WriteLine("Сгенерированная строка");
+                                }
+                            }
+                            using (StreamWriter sw = new StreamWriter(args[1]))
+                            {
+                                using (StreamReader sr = new StreamReader(args[0]))
+                                {
+                                    sw.Write(sr.ReadToEnd());
+                                }
+                            }
+                        }
+
+
+                        SearchFile(args[0], args[1]);
+                        foreach (string arg in list)
+                        {
+                            Console.WriteLine(arg);
+                        }
+
+
+                    }
+
+             Напишите утилиту рекурсивного поиска файлов в заданном каталоге и подкаталогах
+             Пример запуска: utility.exe c:\t file1.txt
+
+                    static List<string> list = new List<string>();
+                    public static void SearchFile(string path, string fileName)
+                    {
+                        list.AddRange(Directory.GetFiles(path, fileName, SearchOption.AllDirectories));
+
+
+                          var files = Directory.GetFiles(path);
+
+                        foreach (var file in files)
+                        {
+                            if (Path.GetFileName(file) == fileName) 
+                            { 
+                                list.Add(file);
+                            }
+                        }
+
+                        foreach (var dir in Directory.GetDirectories(path))
+                        {
+                            SearchFile(dir, fileName);
+                        }
+                        */
+        }
+    }
+}
+
